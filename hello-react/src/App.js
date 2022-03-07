ba87bf7c244d9941f55cee50497cfc6c93d4b4db
sevimozsoy <sevimozsoy30@gmail.com>
2022-03-07 20:39:51 +0300

    if..... RENDER!

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -4,26 +4,32 @@ import React from 'react';
 
 const name = "sevim";
 const surname = "ozsoy";
+const isLoggedIn = false;
 
-function App(){
-// return React.createElement("div",null,"Hello");
-  return(
+function App() {
+  // return React.createElement("div",null,"Hello");
+  return (
     <>
-      <Header/>
+      <Header />
       <p className='denemePTag'>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis atque incidunt earum temporibus nihil possimus, odit aperiam tempore sapiente totam quisquam voluptatum accusantium saepe cupiditate reprehenderit commodi sunt laudantium voluptates.
       </p>
 
       <h1>{name}</h1>
-      <h1>{`Benim adım ${name} soyadım ${surname}`}</h1>
+      <h1>
+        {isLoggedIn ? `Benim adım ${name} soyadım ${surname}` : "Giriş yapmadınız."}
+        {/* {isLoggedIn &&`Benim adım ${name} soyadım ${surname}`}
+      {!isLoggedIn && "Giriş yapmadınız."} */}
+
+      </h1>
 
       <label htmlF or='myInput'>
         Name
         <input type="myInput" /></label>
-        
+
     </>
   )
-  
+
 
 }
 
