b47455af8084ded5470a9b5ebeb4460a7d1433c5
sevimozsoy <sevimozsoy30@gmail.com>
2022-03-07 20:33:27 +0300

    using variables in components

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Header from './components/Header';
 import React from 'react';
 
+const name = "sevim";
+const surname = "ozsoy";
+
 function App(){
 // return React.createElement("div",null,"Hello");
   return(
@@ -11,9 +14,13 @@ function App(){
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis atque incidunt earum temporibus nihil possimus, odit aperiam tempore sapiente totam quisquam voluptatum accusantium saepe cupiditate reprehenderit commodi sunt laudantium voluptates.
       </p>
 
+      <h1>{name}</h1>
+      <h1>{`Benim adım ${name} soyadım ${surname}`}</h1>
+
       <label htmlF or='myInput'>
         Name
         <input type="myInput" /></label>
+        
     </>
   )
   
