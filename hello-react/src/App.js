039f0361a548c28e83d58446eda92f9493267075
sevimozsoy <sevimozsoy30@gmail.com>
2022-03-07 20:26:55 +0300

    jsx basics

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import Header from './components/Header';
+import React from 'react';
 
 function App(){
+// return React.createElement("div",null,"Hello");
   return(
-    <div>
-      <h1>Hello React</h1>
+    <>
       <Header/>
-    </div>
+      <p className='denemePTag'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis atque incidunt earum temporibus nihil possimus, odit aperiam tempore sapiente totam quisquam voluptatum accusantium saepe cupiditate reprehenderit commodi sunt laudantium voluptates.
+      </p>
+
+      <label htmlF or='myInput'>
+        Name
+        <input type="myInput" /></label>
+    </>
   )
+  
+
 }
 
 export default App;
