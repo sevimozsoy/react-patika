1443b96813bc4ef4d7dd1367d42aeca6d2d02c90
sevimozsoy <sevimozsoy30@gmail.com>
2022-03-07 19:59:27 +0300

    created react app in 'hello-react' with create-react-app
    basic component usage

---
+++ hello-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header';
+
+function App(){
+  return(
+    <div>
+      <h1>Hello React</h1>
+      <Header/>
+    </div>
+  )
+}
+
+export default App;
---
+++ hello-react/src/components/Header.js
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <div>
+            Merhaba ben bir header bileşeneyim.
+        </div>
+    )
+}
+
+export default Header;
