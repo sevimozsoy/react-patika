a8352903859062b36a56114fc98f928de4e78339
sevimozsoy <sevimozsoy30@gmail.com>
2022-03-23 20:27:55 +0300

    array states

--- states/src/App.js
+++ states/src/App.js
@@ -2,10 +2,17 @@ import { useState } from "react";
 
 function App() {
   const [name, setName] = useState("Sevim");
+  const[friends, setFriends] = useState(["berkay","emirhan"]);
   return (
     <div className="App">
       <h1>Merhaba {name}</h1>
       <button style={{marginRight: 5}} onClick={() => setName("Selin")}>Change</button>
+      
+      <h2>Friends</h2>
+
+      {friends.map((friend, index) => (
+      <div key={index}>{friend}</div>))}
+      <button onClick={() => setFriends([...friends,"Vin"])}>isim ver</button>   
     </div>
   );
 }
